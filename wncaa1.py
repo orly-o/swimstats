251e14b7d6473b667a9d3b729709890a41f57fcc
Orly <80653731+orly-o@users.noreply.github.com>
2025-04-02 15:06:17 -0400

    switch to py

--- wncaa1.py
+++ wncaa1.py
@@ -1,4 +1,14 @@
 '''
-Women's 2025 NCAA Psych Sheets
-
+Women's 2025 NCAA Swimming Psych Sheets
+Author: Orly Olbum
+Date: April 2, 2025
+Goal: Initial EDA for 2025 ncaa swimming psych sheets
 '''
+
+# libraries
+import pandas as pd
+
+# data load
+wncaa = pd.read_csv('ncaa25_psychsheets.csv')
+print(wncaa.head())
+
